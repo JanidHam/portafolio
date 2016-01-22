805e1f797ba6c051ab6fb5a36ceda1173b77c3a0
JanidHam <janid_ham@hotmail.com>
2016-01-22 15:14:20 -0600

    Se carga el css sin bloquear la pagina. Se agrega correo y telefono al bloque principal.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,4 +6,12 @@ import $ from 'jquery'
 		$('#navbarMenu').toggleClass('header-menu-list--show')
 	})
 
+	let $link = $(`
+		<link rel="stylesheet" type="text/css" 
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css"
+		>
+	`)
+
+	$('head').append($link)
+	
 }())
