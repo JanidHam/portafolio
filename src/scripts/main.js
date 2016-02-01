a9d5db95a399019a36d798c755202dd0187d552f
JanidHam <janid_ham@hotmail.com>
2016-02-01 16:43:22 -0600

    Se describe las funciones en cada trabajo, se agregan los cursos que he tomado.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,9 +13,5 @@ import $ from 'jquery'
 	`)
 
 	$('head').append($link)
-	//var elem = document.createElement('link')
-	//elem.rel = 'stylesheet'
-	//elem.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'
-	//document.head.appendChild(elem)
-
+	
 }())
